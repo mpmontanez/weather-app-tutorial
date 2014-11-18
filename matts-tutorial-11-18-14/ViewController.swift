e5c1d74f12e119857040801fc0021b75aa965290
Matthew Montanez <matthew.montanez@demandmedia.com>
2014-11-18 06:56:51 -0800

    Initial Commit

---
+++ matts-tutorial-11-18-14/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  matts-tutorial-11-18-14
+//
+//  Created by Matthew Montanez on 11/18/14.
+//  Copyright (c) 2014 Matthew Montanez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
